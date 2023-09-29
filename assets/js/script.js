bdb76d4cb80c020cd183a75d6032a80c2eeed3ba
sdthomas91 <>
2023-09-29 13:19:56 +0000

    added some js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You click ${gameType}`);
+            }
+        });
+    }
+});
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+
 function runGame() {
 
+    //creates random number between 1 and 25
+
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
 }
+
 
 function checkAnswer() {
 
