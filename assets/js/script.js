595340c19601e42b86e76afde119c75aece801e6
sdthomas91 <>
2023-09-29 12:51:00 +0000

    basic css and js function carcass added

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
+
